dd09054c8fc8b7e8de33f21c7f1ecd0a3ab02e8b
eunguru <eunguru1020@gmail.com>
2020-02-23 00:46:37 +0900

    test

--- Sacred/hello.py
+++
@@ -1,7 +0,0 @@
-from sacred import Experiment
-
-ex = Experiment()
-
-@ex.automain
-def my_main():
-    print("hello world")
--- Sacred/test.py
+++
@@ -1,9 +0,0 @@
-from sacred import Experiment
-
-# create experiment instance
-ex = Experiment()
-
-# decorate
-@ex.automain
-def my_main():
-    print("Hello world")
